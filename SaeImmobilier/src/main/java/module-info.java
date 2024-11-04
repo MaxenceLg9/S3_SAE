6226d886a46a3eca5ef2f1939920a86fe38ad8ff
Maxence Lagourgue <maxence.lagourgue@free.fr>
2024-11-04 16:03:55 +0100

    First commit : FX init

---
+++ SaeImmobilier/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module net.mpvm.saeimmobilier.saeimmobilier {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+//    requires org.controlsfx.controls;
+//    requires com.dlsc.formsfx;
+//    requires net.synedra.validatorfx;
+//    requires org.kordamp.ikonli.javafx;
+//    requires org.kordamp.bootstrapfx.core;
+//    requires eu.hansolo.tilesfx;
+//    requires com.almasb.fxgl.all;
+
+    opens net.mpvm.saeimmobilier.saeimmobilier to javafx.fxml;
+    exports net.mpvm.saeimmobilier.saeimmobilier;
+}
