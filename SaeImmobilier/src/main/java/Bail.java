b9969a3adb8f45d5edfc0834ed5bbecb2d61a0dc
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2024-11-08 15:30:16 +0100

    Update Bail.java

--- SaeImmobilier/src/main/java/Bail.java
+++ SaeImmobilier/src/main/java/Bail.java
@@ -1,15 +1,18 @@
+package modele;
 
-public class Bail {
+public class Bail extends Date{
 	private int nbMoisLoues;
 	private float provisionSurCharge;
 	private float factureEau;
 	private float totalCharge;
 	private float loyer;
-	private float regularitationCharge;
-	private Date date;
+	private float regularisationCharge;
+	private Date dateDebut;
+	private Date dateFin;
+	
 
-	public Bail(Date date) {
-		this.date = date;
+	public Bail(Date dateDebut) {
+		this.date = dateDebut;
 	}
 
 	public int getNbMoisLoues() {
@@ -60,12 +63,24 @@ public class Bail {
 		this.regularitationCharge = regularitationCharge;
 	}
 
-	public Date getDate() {
-		return this.date;
+	public Date getDateDebut() {
+		return this.dateDebut;
 	}
 
-	public void setDate(Date date) {
-		this.date = date;
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
 	}
-
+	public Date getDateFin() {
+		int nbMois=this.nbMoisLoues;
+		int anneeARajouter=0;
+		while (!this.getNbMoisLoues()+this.dateDebut.getMois()<=12) {
+			int nbMois=nbMois-12;
+			int anneeARajouter=+1;
+		}	
+		
+		this.dateFin=this.dateDebut.setMois(this.dateDebut.getMois()+this.getNbMoisLoues());
+		return this.dateFin;
+		
+	}
+	
 }
