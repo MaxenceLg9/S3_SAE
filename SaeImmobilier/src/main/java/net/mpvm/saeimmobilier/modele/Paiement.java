7829478905aad28e3994a736024cb4763be43975
Sockeel Philemon <philemon.sockeel@etu.iut-tlse3.fr>
2024-11-19 12:01:19 +0100

    Update Paiement setteur, getteur et constructeurs

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Paiement.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Paiement.java
@@ -1,8 +1,59 @@
 package modele;
 
+import java.util.Date;
+
 public class Paiement {
-    private float Montant;
-    private Date DatePaiement;
+    private float montant;
+    private Date datePaiement;
     private TypePaiement typePaiement;
     private StatutPaiement statutPaiement;
+
+    // Constructeur privé qui demande tous les attributs
+    private Paiement(float montant, Date datePaiement, TypePaiement typePaiement, StatutPaiement statutPaiement) {
+        this.montant = montant;
+        this.datePaiement = datePaiement;
+        this.typePaiement = typePaiement;
+        this.statutPaiement = statutPaiement;
+    }
+
+    // Constructeur public qui demande tous les attributs sauf statutPaiement
+    public Paiement(float montant, Date datePaiement, TypePaiement typePaiement) {
+        this.montant = montant;
+        this.datePaiement = datePaiement;
+        this.typePaiement = typePaiement;
+    }
+
+    // Getters
+    public float getMontant() {
+        return montant;
+    }
+
+    public Date getDatePaiement() {
+        return datePaiement;
+    }
+
+    public TypePaiement getTypePaiement() {
+        return typePaiement;
+    }
+
+    public StatutPaiement getStatutPaiement() {
+        return statutPaiement;
+    }
+
+    // Setters
+    public void setMontant(float montant) {
+        this.montant = montant;
+    }
+
+    public void setDatePaiement(Date datePaiement) {
+        this.datePaiement = datePaiement;
+    }
+
+    public void setTypePaiement(TypePaiement typePaiement) {
+        this.typePaiement = typePaiement;
+    }
+
+    public void setStatutPaiement(StatutPaiement statutPaiement) {
+        this.statutPaiement = statutPaiement;
+    }
 }
