f4211f1dbfaa7c659d0262b5ecdc90d17bb5cc3e
Sockeel Philemon <philemon.sockeel@etu.iut-tlse3.fr>
2024-11-19 12:01:57 +0100

    Update Quittance setteur, getteur et constructeurs

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Quittance.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Quittance.java
@@ -1,7 +1,49 @@
 package modele;
 
-public class Quittance extends Document{
-    private float MontantLoyer;
-    private float MontantCharges;
+public class Quittance extends Document {
+    private float montantLoyer;
+    private float montantCharges;
     private Paiement paiement;
+
+    // Constructeur privé qui demande tous les attributs (y compris ceux de la classe parente)
+    private Quittance(String cheminDocument, Date dateAjout, float montantLoyer, float montantCharges, Paiement paiement) {
+        super(cheminDocument, dateAjout); // Appelle le constructeur de la classe parente
+        this.montantLoyer = montantLoyer;
+        this.montantCharges = montantCharges;
+        this.paiement = paiement;
+    }
+
+    // Constructeur public qui demande tous les attributs sauf ceux de la classe parente
+    public Quittance(String cheminDocument, Date dateAjout, float montantLoyer, float montantCharges, Paiement paiement) {
+        super(cheminDocument, dateAjout); // Appelle le constructeur de la classe parente
+        this.montantLoyer = montantLoyer;
+        this.montantCharges = montantCharges;
+        this.paiement = paiement;
+    }
+
+    // Getters
+    public float getMontantLoyer() {
+        return montantLoyer;
+    }
+
+    public float getMontantCharges() {
+        return montantCharges;
+    }
+
+    public Paiement getPaiement() {
+        return paiement;
+    }
+
+    // Setters
+    public void setMontantLoyer(float montantLoyer) {
+        this.montantLoyer = montantLoyer;
+    }
+
+    public void setMontantCharges(float montantCharges) {
+        this.montantCharges = montantCharges;
+    }
+
+    public void setPaiement(Paiement paiement) {
+        this.paiement = paiement;
+    }
 }
