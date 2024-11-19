763fd536aaed5b1afb9a84fc32897184563718f9
Sockeel Philemon <philemon.sockeel@etu.iut-tlse3.fr>
2024-11-19 11:58:27 +0100

    Update Document setteur, getteur et constructeur

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Document.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Document.java
@@ -1,8 +1,44 @@
 package modele;
 
-public class Document {
-    private int IdDocument;
-    private String CheminDocument;
-    private Date DateAjout;
+import java.util.Date;
 
+public class Document {
+    private int idDocument;
+    private String cheminDocument;
+    private Date dateAjout;
+
+    // Constructeur privé qui demande tous les attributs
+    private Document(int idDocument, String cheminDocument, Date dateAjout) {
+        this.idDocument = idDocument;
+        this.cheminDocument = cheminDocument;
+        this.dateAjout = dateAjout;
+    }
+
+    // Constructeur public qui demande tous les attributs sauf idDocument
+    public Document(String cheminDocument, Date dateAjout) {
+        this.cheminDocument = cheminDocument;
+        this.dateAjout = dateAjout;
+    }
+
+    // Getters
+    public int getIdDocument() {
+        return idDocument;
+    }
+
+    public String getCheminDocument() {
+        return cheminDocument;
+    }
+
+    public Date getDateAjout() {
+        return dateAjout;
+    }
+
+    // Setters
+    public void setCheminDocument(String cheminDocument) {
+        this.cheminDocument = cheminDocument;
+    }
+
+    public void setDateAjout(Date dateAjout) {
+        this.dateAjout = dateAjout;
+    }
 }
