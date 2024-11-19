f92877efe9354c0d8a2b3a9148a903eab4e26ff8
Sockeel Philemon <philemon.sockeel@etu.iut-tlse3.fr>
2024-11-19 11:59:48 +0100

    Update EtatDesLieux setteur, getteur et constructeur

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/EtatDesLieux.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/EtatDesLieux.java
@@ -1,7 +1,35 @@
 package modele;
 
-public class EtatDesLieux extends Document{
-    private int IdEtatDesLieux;
-    private Date DateSignature;
+import java.util.Date;
 
+public class EtatDesLieux extends Document {
+    private int idEtatDesLieux;
+    private Date dateSignature;
+
+    // Constructeur privé qui demande tous les attributs (y compris ceux de la classe parente)
+    private EtatDesLieux(int idEtatDesLieux, String cheminDocument, Date dateAjout, Date dateSignature) {
+        super(cheminDocument, dateAjout); // Appelle le constructeur de la classe parente
+        this.idEtatDesLieux = idEtatDesLieux;
+        this.dateSignature = dateSignature;
+    }
+
+    // Constructeur public qui demande tous les attributs sauf idEtatDesLieux
+    public EtatDesLieux(String cheminDocument, Date dateAjout, Date dateSignature) {
+        super(cheminDocument, dateAjout); // Appelle le constructeur de la classe parente
+        this.dateSignature = dateSignature;
+    }
+
+    // Getters
+    public int getIdEtatDesLieux() {
+        return idEtatDesLieux;
+    }
+
+    public Date getDateSignature() {
+        return dateSignature;
+    }
+
+    // Setters
+    public void setDateSignature(Date dateSignature) {
+        this.dateSignature = dateSignature;
+    }
 }
