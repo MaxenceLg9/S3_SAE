7a9472f5de32ed9783d68ed0bf00bb890221f0d1
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2024-11-22 14:36:38 +0100

    type contrat

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Locataire.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Locataire.java
@@ -1,34 +1,42 @@
 package net.mpvm.saeimmobilier.modele;
+
+import net.mpvm.saeimmobilier.sql.BD;
+
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Locataire {
-
+	private int idLocataire;
+	public static final String TABLE_NAME = "Locataire";
 	private int numero;
-	private String genre;
-	private String telepone;
+	private char sexe;
+	private String telephone;
 	private String email;
 	private String nom;
 	private String prenom;
-	private ArrayList<Bail> baux;
-
+	private final ArrayList<Bail> baux;
+	private final ArrayList<Float> charges;
 	private float totalCharge;
 
-	private Locataire(int numero, String nom, String prenom, String email) {
+	private Locataire(int idLocataire,String nom, String prenom, String email) {
+		this.idLocataire = idLocataire;
 		this.email = email;
-		this.numero = numero;
 		this.nom = nom;
 		this.prenom = prenom;
 		this.baux=new ArrayList<>();
+		this.charges=new ArrayList<>();
 		this.totalCharge=0f;
 	}
 
-	public Locataire(String nom, String prenom, String email){
-		this.email = email;
-		this.nom = nom;
-		this.prenom = prenom;
-		this.baux=new ArrayList<>();
-		this.totalCharge=0f;
-	}
+	public Locataire(String nom, String prenom, String email, char sexe, String telephone) {
+		this.nom=nom ;
+		this.prenom=prenom;
+		this.email=email;
+		this.sexe = sexe;
+		this.telephone = telephone;
+        this.baux = new ArrayList<>();
+        this.charges = new ArrayList<>();
+    }
 
 	public int getNumero() {
 		return this.numero;
@@ -38,20 +46,20 @@ public class Locataire {
 		this.numero = numero;
 	}
 
-	public String getGenre() {
-		return this.genre;
+	public char getSexe() {
+		return this.sexe;
 	}
 
-	public void setGenre(String genre) {
-		this.genre = genre;
+	public void setSexe(char sexe) {
+		this.sexe = sexe;
 	}
 
-	public String getTelepone() {
-		return this.telepone;
+	public String getTelephone() {
+		return this.telephone;
 	}
 
 	public void setTelepone(String telepone) {
-		this.telepone = telepone;
+		this.telephone = telepone;
 	}
 
 	public String getEmail() {
@@ -84,11 +92,27 @@ public class Locataire {
 		this.baux.add(bail);
 	}
 
+	public ArrayList<Float> getCharges(){return this.charges;}
+
+	public void setCharges(float charges) {this.charges.add(charges);}
+
 	public float getTotalCharge(){return this.totalCharge;}
 
 	public void setTotalCharge(float totalCharge) {this.totalCharge = totalCharge;}
 
-	public void setBaux(ArrayList<Bail> baux) {
-		this.baux = baux;
+	public int getIdLocataire() {
+		return idLocataire;
+	}
+	public void setIdLocataire(int idLocataire) {
+		this.idLocataire = idLocataire;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public void save() {
+		Map<String, String> params = Map.of("nom", this.nom, "prenom", this.prenom, "email", this.email, "sexe", Character.toString(this.sexe), "telephone", this.telephone);
+		BD.insertInto(TABLE_NAME, params, true);
 	}
 }
