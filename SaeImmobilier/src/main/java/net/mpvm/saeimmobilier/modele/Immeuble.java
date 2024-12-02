514ab85493a88032714b89fcd459a5c4cc71d91b
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2024-12-02 15:22:20 +0100

    vue bien et auth

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Immeuble.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Immeuble.java
@@ -1,25 +1,12 @@
 package net.mpvm.saeimmobilier.modele;
-import net.mpvm.saeimmobilier.sql.Query.QueryElement;
-import net.mpvm.saeimmobilier.sql.Query.Queryable;
-import net.mpvm.saeimmobilier.sql.Query.SelectQueryElement;
-import net.mpvm.saeimmobilier.sql.Query.UpdateQueryElement;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+
 
 public class Immeuble extends Bien{
-
-	public static final String INSERT_QUERY = "INSERT INTO immeuble (Adresse, Ville, CodePostal) VALUES (?, ?, ?)";
-	public static final String SELECT_QUERY = "SELECT * FROM immeuble";
-	public static final String SELECT_WHERE_QUERY = "SELECT * FROM immeuble WHERE Adresse = ? AND Ville = ? AND CodePostal = ?";
-	public static final String DELETE_QUERY = "DELETE FROM immeuble WHERE idImmeuble = ?";
-	public static final String UPDATE_QUERY = "UPDATE immeuble SET Adresse = ?, Ville = ?, CodePostal = ?";
-	public static final String SELECT_FROM_BIENLOUABLE = "SELECT * FROM bienlouable";
-
-
 	private int idImmeuble;
 	private List<BienLouable> biensAssocies;
 	private List<Travaux> travauxAssocies;
@@ -32,21 +19,9 @@ public class Immeuble extends Bien{
 	}
 
 	public Immeuble(String ville, int codePostal, String adresse) {
-
 		super(ville, codePostal, adresse); // Utilisation du constructeur de Bien
-		this.getIdImmeuble();
 		this.biensAssocies = new ArrayList<>();
 		this.travauxAssocies = new ArrayList<>();
-	}
-
-	@Override
-	public TypeBien getTypeBien() {
-		return TypeBien.IMMEUBLE;
-	}
-
-	@Override
-	public String getTypeBienString() {
-		return "IMMEUBLE";
 	}
 
 
@@ -57,16 +32,7 @@ public class Immeuble extends Bien{
 	}
 
 	public void setIdImmeuble(int idImmeuble) {
-		try(SelectQueryElement query= new SelectQueryElement(SELECT_WHERE_QUERY)){
-			query.setArgs(
-					Map.of(
-							1,this.getAdresse(),
-							2,this.getVille(),
-							3,this.getCodePostal()
-					)).execute().getInt("idImmeuble");
-		} catch (QueryElement.QueryException | SQLException e) {
-			throw new RuntimeException(e);
-		}
+		this.idImmeuble = idImmeuble;
 	}
 
 
@@ -82,69 +48,18 @@ public class Immeuble extends Bien{
 	public void setTravauxAssocies(List<Travaux> travauxAssocies) {
 		this.travauxAssocies = travauxAssocies;
 	}
+	static Immeuble mapResultSetToImmeuble(ResultSet resultSet) throws SQLException, SQLException {
+		int idImmeuble = resultSet.getInt("IdBien"); // Si IdBien correspond à l'identifiant unique de l'immeuble
+		String ville = resultSet.getString("Ville");
+		int codePostal = resultSet.getInt("CodePostal");
+		String adresse = resultSet.getString("Adresse");
 
-	public List<BienLouable> getBiensAssocies() throws BienException {
-		try (SelectQueryElement query = new SelectQueryElement(SELECT_FROM_BIENLOUABLE)) {
-			query.setArgs(Map.of(1, this.idImmeuble));
-			ResultSet rs = query.execute();
-			while (rs.next()) {
-				BienLouable bien;
-				if(rs.getString("TypeBien").equals("Habitation")) {
-					bien = new Habitation(
-							rs.getString("Ville"),
-							rs.getInt("CodePostal"),
-							rs.getString("Adresse"),
-							rs.getInt("NbPieces"),
-							rs.getString("NumeroFiscal"),
-							null,
-							rs.getFloat("Surface"),
-							rs.getDate("DateAjout"));
-					biensAssocies.add(bien);
-				}else{
-					bien = new Garage(rs.getString("Ville"),
-							rs.getInt("CodePostal"),
-							rs.getString("Adresse"),
-							rs.getInt("NbPieces"),
-							rs.getString("NumeroFiscal"),
-							null,
-							rs.getFloat("Surface"),
-							rs.getDate("DateAjout"));
-					biensAssocies.add(bien);
-				}
-			}
-		} catch (QueryElement.QueryException | SQLException e) {
-			throw new BienException("Erreur lors de la récupération des biens associés", e);
-		}
-		return biensAssocies;
+		return new Immeuble(ville, codePostal, adresse, idImmeuble);
 	}
+
+	// Méthode : récupérer les biens louables d’un immeuble
 	@Override
 	public String toString(){
 		return this.getAdresse() + " " + this.getVille() + ", " + this.getCodePostal();
-	}
-
-	@Override
-	public void save() throws QueryableException {
-		if(this.getIdImmeuble() != -1)
-			throw new Bien.QueryableException("Le bien existe déjà dans la table");
-		try(UpdateQueryElement q = new UpdateQueryElement(INSERT_QUERY, true)){
-			q.setArgs(
-							Map.of(1, this.getAdresse(),
-									2, this.getVille(),
-									3, this.getCodePostal()))
-					.execute();
-		}
-		catch (QueryElement.QueryException sqlE){
-			throw new Queryable.QueryableException("Erreur lors de l'ajout du bien");
-		}
-	}
-
-	@Override
-	public void modify() throws QueryableException {
-
-	}
-
-	@Override
-	public void delete() throws QueryableException {
-
 	}
 }
