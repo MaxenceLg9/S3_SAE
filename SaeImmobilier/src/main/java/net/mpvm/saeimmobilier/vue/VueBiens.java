9f44eb93d8de4ac53e80d9dfd39df1de9b4ffbfa
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2024-12-02 13:21:40 +0100

    vue bien et auth

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueBiens.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueBiens.java
@@ -1,4 +1,16 @@
 package net.mpvm.saeimmobilier.vue;
 
-public class VueBiens {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import net.mpvm.saeimmobilier.util.JfxUtil;
+
+public class VueBiens extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        JfxUtil.applicationInit(primaryStage, "viewbiens.fxml", "Liste des Biens");
+    }
 }
