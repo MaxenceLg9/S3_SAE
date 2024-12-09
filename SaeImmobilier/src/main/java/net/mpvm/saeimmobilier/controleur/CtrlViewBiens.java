82a528714f2ca8278e2577c522d3e947177201ac
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2024-12-09 16:02:43 +0100

    Fond d'écran bonne couleur + ratio

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/controleur/CtrlViewBiens.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/controleur/CtrlViewBiens.java
@@ -31,7 +31,7 @@ public class CtrlViewBiens {
     private void afficheBiens() {
         try {
             biens = Bien.findAll().stream().collect(Collectors.toMap(Bien::getIdBien, Function.identity()));
-        } catch (Queryable.QueryableException e) {
+        } catch (Bien.BienException e) {
             biens = new HashMap<>();
         }
         vBoxContent.getChildren().clear();
