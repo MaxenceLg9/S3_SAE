85f9336dc00c6cad516cd582a065aadd09cf4ba8
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2024-12-09 14:52:38 +0100

    Connection bd bon mdp

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/sql/Connection/BD.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/sql/Connection/BD.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class BD{
 
     public static Connection getConnection(boolean commit) throws SQLException {
-        String url = "jdbc:mysql://127.0.0.5:3306/bdimmo";
-        Connection connection = DriverManager.getConnection(url,"root","31Mai2005*");
+        String url = "jdbc:mysql://localhost/bdimmo";
+        Connection connection = DriverManager.getConnection(url,"root","iutinfo");
         connection.setAutoCommit(commit);
         return connection;
     }
