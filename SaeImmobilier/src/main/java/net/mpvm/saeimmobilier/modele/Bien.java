2ee1dd2d3d7aece18e791875c68f7887e68e0b17
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2024-12-09 10:32:06 +0100

    Normalement y a plus d'erreur

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Bien.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Bien.java
@@ -11,7 +11,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 
 public abstract class Bien implements Queryable {
