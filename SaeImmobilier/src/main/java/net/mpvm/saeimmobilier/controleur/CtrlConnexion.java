3247375f57b18a68a0775ab70d22e54a981cd5ef
Zeynnz <zzeynn8@gmail.com>
2024-12-13 18:20:53 +0100

    Correction ajout immeuble Correction inscription et le bouton annuler Correction pour la connexion

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/controleur/CtrlConnexion.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/controleur/CtrlConnexion.java
@@ -90,7 +90,7 @@ public class CtrlConnexion {
             alertPwdEmpty();
         } else if (isValidEmail(this.FieldMail.getText())){
             try {
-                Map<String,Proprietaire> proprietaires = Proprietaire.findAll().stream().filter(Proprietaire-> Proprietaire.getEmail().equals(this.FieldMail.getText())).collect(Collectors.toMap(Proprietaire::getMotDePasse, Function.identity()));
+                Map<String,Proprietaire> proprietaires = Proprietaire.findAll().stream().collect(Collectors.toMap(Proprietaire::getMotDePasse, Function.identity()));
                 for(Proprietaire p : proprietaires.values()) {
                     System.out.println(this.FieldMail.getText());
                     System.out.println(p.getEmail());
@@ -102,6 +102,7 @@ public class CtrlConnexion {
                             } catch (Exception e) {
                                 throw new RuntimeException(e);
                             }
+
                             Stage stageActu = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
                             stageActu.close();
 
