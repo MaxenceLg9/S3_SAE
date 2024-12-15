da0cb6b7d5035aae2bb4c5bb6cb9dad563e7de31
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2024-12-15 22:37:46 +0100

    removing bloat (useless content)

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/TypeBien.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/TypeBien.java
@@ -2,9 +2,4 @@ package net.mpvm.saeimmobilier.modele;
 
 public enum TypeBien {
     HABITATION,GARAGE, IMMEUBLE;
-
-    private String designation;
-    TypeBien() {
-        this.designation = designation;
-    }
 }
