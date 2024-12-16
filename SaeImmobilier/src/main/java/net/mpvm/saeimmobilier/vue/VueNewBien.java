b9285fa88df0543d92a944c6a6e73ae731c645d2
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2024-12-16 15:40:15 +0100

    pour mettre à jour sur moi

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueNewBien.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueNewBien.java
@@ -1,9 +1,7 @@
 package net.mpvm.saeimmobilier.vue;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 import net.mpvm.saeimmobilier.util.JfxUtil;
 
