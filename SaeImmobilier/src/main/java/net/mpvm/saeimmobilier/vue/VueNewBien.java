9628e5db3b6a4b029e60bc41be8884513796f188
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2024-12-16 10:21:18 +0100

    taille des fenêtres correcte

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueNewBien.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueNewBien.java
@@ -9,14 +9,15 @@ import net.mpvm.saeimmobilier.util.JfxUtil;
 
 public class VueNewBien extends Application {
 
-    private static final int DEFAULT_WIDTH = 800;
+    private static final int DEFAULT_WIDTH = 850;
     private static final int DEFAULT_HEIGHT = 750;
 
     @Override
     public void start(Stage stage) throws Exception {
 
         JfxUtil.applicationInit(stage, "newbien.fxml", "Ajouter un Bien", DEFAULT_HEIGHT, DEFAULT_WIDTH);
-        stage.setResizable(false);
+        stage.setMinWidth(DEFAULT_WIDTH);
+        stage.setMinHeight(DEFAULT_HEIGHT);
     }
 
     public static void showWindow(Stage stage) throws Exception {
