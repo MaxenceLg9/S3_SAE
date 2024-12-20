f0454156dd8b527626aec238c378c0a9645cc3f1
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2024-12-20 16:16:23 +0100

    changing structure of importants class : adding builder class with build method & some fields that reference field from main classes. Changing all the construction to that

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Habitation.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Habitation.java
@@ -1,70 +1,86 @@
 package net.mpvm.saeimmobilier.modele;
 
 import net.mpvm.saeimmobilier.sql.Query.QueryElement;
-import net.mpvm.saeimmobilier.sql.Query.Queryable;
-import net.mpvm.saeimmobilier.sql.Query.UpdateQueryElement;
+import net.mpvm.saeimmobilier.sql.Query.SelectQueryElement;
 
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public final class Habitation extends BienLouable {
 
-    public static final String INSERT_QUERY = "INSERT INTO bien (ComplementAdresse, Adresse, Ville, CodePostal, TypeBien, Surface, NombrePieces, NumeroFiscal, DateAjout) VALUES (?, ?, ?, ?, ?,?,?,?,?)";
+    public static final String SELECT_QUERY = "SELECT * FROM Bien WHERE TypeBien = 'Habitation'";
+
+    private static final Map<Integer,Habitation> habitations = new HashMap<>();
 
     @Override
     public TypeBien getTypeBien() {
         return TypeBien.HABITATION;
     }
 
-
-    public Habitation(String complementAdresse, int nbPieces, String NumeroFiscal, Immeuble immeuble, float surface, Date dateAjout) {
-        this(complementAdresse, nbPieces,NumeroFiscal,immeuble,surface,dateAjout, -1);
-    }
-
-    private Habitation(String complementAdresse, int nbPieces, String NumeroFiscal, Immeuble immeuble, float surface, Date dateAjout, int idBien) {
+    private Habitation(String complementAdresse, int nbPieces, String NumeroFiscal, Immeuble immeuble, float surface, Date dateAjout, int idBien) throws BienException {
         super(complementAdresse, nbPieces,NumeroFiscal,immeuble,surface,dateAjout, idBien);
-    }
-
-    Habitation(ResultSet rs) throws SQLException, Immeuble.ImmeubleException {
-        this(rs.getString("complementAdresse"),
-                rs.getInt("NombrePieces"),
-                rs.getString("NumeroFiscal"),
-                Immeuble.getFromId(rs.getInt("IdImmeuble")),
-                rs.getFloat("Surface"),
-                rs.getDate("DateAjout"),
-                rs.getInt("IdBien"));
-    }
-
-    @Override
-    public void save() throws Habitation.HabitationException {
-        if(this.getIdBien() != -1)
-            throw new Habitation.HabitationException("Le bien existe déjà !");
-        try(UpdateQueryElement query = new UpdateQueryElement(INSERT_QUERY, true)){
-            query.setArgs(
-                    Map.of(1,this.getComplementAdresse(),
-                            2, this.getAdresse(),
-                            3, this.getVille(),
-                            4, this.getCodePostal(),
-                            5, this.getTypeBienString(),
-                            6, this.getSurface(),
-                            7, this.getNbPieces(),
-                            8, this.getNumeroFiscal(),
-                            9, this.getDateAjout()
-                    )).execute();
+        if((!habitations.containsKey(this.getIdBien()) || habitations.get(this.getIdBien()) == null) && idBien != -1){
+            habitations.put(this.getIdBien(),this);
         }
-        catch (QueryElement.QEltException queryException){
-            queryException.getSqlException().printStackTrace();
-            throw new Habitation.HabitationException("Erreur lors de l'ajout du bien", queryException.getSqlException());
+    }
+
+    private Habitation(HBuilder hBuilder) throws BienException {
+        this(hBuilder.getComplementAdresse(),hBuilder.getNbPieces(),hBuilder.getNumeroFiscal(),hBuilder.getImmeuble(),hBuilder.getSurface(),hBuilder.getDateAjout(),hBuilder.getIdBien());
+    }
+
+    public static List<Habitation> findAll() throws HabitationException {
+        List<Habitation> habitations = new LinkedList<>();
+        try(SelectQueryElement selectQueryElement = new SelectQueryElement(SELECT_QUERY)){
+            selectQueryElement.execute();
+            List<Map<String,Object>> result = selectQueryElement.getResult();
+            for(Map<String, Object> row : result){
+                habitations.add(new Habitation.HBuilder(row).build());
+            }
+            return habitations;
+        }catch(QueryElement.QEltException qEltException){
+            qEltException.printStackTrace();
+            throw new HabitationException("Impossible de récupérer les habitations : " + qEltException.getSqlException().getMessage(), qEltException.getSqlException());
+        }
+    }
+
+    public void save() throws BienException {
+        super.save();
+        habitations.put(this.getIdBien(), this);
+    }
+
+    public static class HBuilder extends BLBuilder {
+        public HBuilder(String complementAdresse, int nbPieces, String NumeroFiscal, Immeuble immeuble, float surface, Date dateAjout) {
+            this(complementAdresse, nbPieces,NumeroFiscal,immeuble,surface,dateAjout, -1);
+        }
+
+        HBuilder(String complementAdresse, int nbPieces, String NumeroFiscal, Immeuble immeuble, float surface, Date dateAjout, int idBien) {
+            super(complementAdresse, nbPieces,NumeroFiscal,immeuble,surface,dateAjout, idBien);
+        }
+
+        HBuilder(Map<String, Object> args) throws BienException {
+            this(args.get("ComplementAdresse").toString(),
+                    (int) args.get("NombrePieces"),
+                    args.get("NumeroFiscal").toString(),
+                    new Immeuble.IBuilder((int) args.get("IdImmeuble")).build(),
+                    ((Double) args.get("Surface")).floatValue(),
+                    (Date) args.get("DateAjout"),
+                    (int) args.get("IdBien"));
+        }
+
+        @Override
+        public Habitation build() throws BienException {
+            if(habitations.containsKey(this.getIdBien()) && habitations.get(this.getIdBien()) != null)
+                return habitations.get(this.getIdBien());
+            return new Habitation(this);
         }
     }
 
     public static class HabitationException extends BienException{
-
-        public HabitationException(String message) {
-            this(message,null);
-        }
 
         public HabitationException(String message, SQLException sqlException) {
             super(message, sqlException);
