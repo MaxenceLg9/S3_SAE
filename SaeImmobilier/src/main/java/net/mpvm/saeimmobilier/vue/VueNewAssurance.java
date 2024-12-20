132f0c56c61c0b15ca89ad55cda4bbe1fb33cbf1
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2024-12-20 15:38:27 +0100

    view new assurance sans le double show window

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueNewAssurance.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueNewAssurance.java
@@ -15,8 +15,5 @@ public class VueNewAssurance extends Application {
         vue.start(stage);
     }
 
-    public static void showWindow(Stage stage) throws Exception {
-        VueNewAssurance vue = new VueNewAssurance();
-        vue.start(stage);
-    }
+
 }
