ea35d5addd28c2390fc30e8b05e067066fc6b75d
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2024-12-24 12:04:31 +0100

    class to final

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/sql/Query/Result.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/sql/Query/Result.java
@@ -3,6 +3,6 @@ package net.mpvm.saeimmobilier.sql.Query;
 import java.util.LinkedList;
 import java.util.Map;
 
-public class Result extends LinkedList<Map<String,Object>> {
+public final class Result extends LinkedList<Map<String,Object>> {
 
 }
