c39fe03963617db517e053a2558866947a35d311
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2024-12-25 23:52:46 +0100

    changing size of scene & window

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/util/JfxUtil.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/util/JfxUtil.java
@@ -2,26 +2,14 @@ package net.mpvm.saeimmobilier.util;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.css.Size;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import net.mpvm.saeimmobilier.controleur.CtrlAccueil;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
-import java.util.Timer;
-
-import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
-import static javafx.scene.layout.Region.USE_PREF_SIZE;
 
 public class JfxUtil {
     public static void applicationInit(Stage primaryStage, String fxmlFile, String nomPage, double height, double width) {
