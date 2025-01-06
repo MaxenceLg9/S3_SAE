54afb0da85dbbb845896d58d7ba50ad9eb810168
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2025-01-06 12:12:12 +0100

    Attribuer assurance avec des trucs en plus

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueAttribuerAssurance.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueAttribuerAssurance.java
@@ -4,9 +4,13 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import net.mpvm.saeimmobilier.util.JfxUtil;
 
-public class VueAttribuerAssurance extends Application {
-    @Override
-    public void start(Stage stage) {
+public class VueAttribuerAssurance  {
+
+    public void startforBien(Stage stage,int bienId) {
         JfxUtil.updateStage(stage, "attribuerassurance.fxml","Attribuer une Assurance",700,800);
+    }
+    public static void showWindow(Stage stage, int bienId) throws Exception {
+        VueAttribuerAssurance vue = new VueAttribuerAssurance();
+        vue.startforBien(stage, bienId);
     }
 }
