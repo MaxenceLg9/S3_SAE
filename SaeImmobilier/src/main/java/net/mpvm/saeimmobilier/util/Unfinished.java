e376f7980cddef587146888592b95621dcebcb11
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-01-06 16:32:38 +0100

    adding source

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/util/Unfinished.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/util/Unfinished.java
@@ -1,11 +1,14 @@
 package net.mpvm.saeimmobilier.util;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  * Annotation to mark unfinished code
  */
+@Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.METHOD, ElementType.TYPE,ElementType.CONSTRUCTOR})
 public @interface Unfinished {
     
