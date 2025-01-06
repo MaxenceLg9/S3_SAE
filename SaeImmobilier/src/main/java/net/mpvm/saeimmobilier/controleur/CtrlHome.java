534650ea4e891ef0f22f2d805b8a4fd9d3f52765
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-01-06 12:21:11 +0100

    improvments of window & tablePane

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/controleur/CtrlHome.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/controleur/CtrlHome.java
@@ -1,15 +1,51 @@
 package net.mpvm.saeimmobilier.controleur;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import net.mpvm.saeimmobilier.modele.Assurance;
+import net.mpvm.saeimmobilier.modele.Immeuble;
+import net.mpvm.saeimmobilier.modele.Locataire;
+import net.mpvm.saeimmobilier.sql.Query.Queryable;
 import net.mpvm.saeimmobilier.util.JfxUtil;
 import net.mpvm.saeimmobilier.vue.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class CtrlHome {
+
+    @FXML
+    public TabPane tabPane;
+
+    public void initialize(){
+        tabPane.getTabs().forEach(tab -> {
+            tab.setContent(new VBox(5));
+            try {
+                if("Immeubles".equals(tab.getText())){
+                    Immeuble.findAll().forEach(immeuble -> {
+                        ((VBox) tab.getContent()).getChildren().add(new Label(immeuble.toString()));
+                    });
+                } else if("Locataires".equals(tab.getText())){
+                    Locataire.findAll().forEach(locataire -> {
+                        ((VBox) tab.getContent()).getChildren().add(new Label(locataire.toString()));
+                    });
+                } else if("Assurances".equals(tab.getText())){
+                    Assurance.findAll().forEach(assurance -> {
+                        ((VBox) tab.getContent()).getChildren().add(new Label(assurance.toString()));
+                    });
+                }
+            } catch (Queryable.QbleException e) {
+                JfxUtil.displayError("Erreur",e.getMessage());
+            }
+        });
+
+    }
 
     @FXML
     public void ajouterBien(ActionEvent event){
