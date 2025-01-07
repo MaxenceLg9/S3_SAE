37f0bcf903253a403e50d3a1e2edb7aba5a4d55d
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2025-01-07 10:19:27 +0100

    jsp

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueBails.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueBails.java
@@ -8,7 +8,7 @@ import static javafx.application.Application.launch;
 public class VueBails {
     private int IdBien;
 
-    public void startForImmeuble(Stage stage,int IdBien) {
+    public void startForBiensLouables(Stage stage, int IdBien) {
         this.IdBien = IdBien;
         JfxUtil.updateStage(stage, "viewBails.fxml", "Locations du Bien " + IdBien,700,800);
     }
