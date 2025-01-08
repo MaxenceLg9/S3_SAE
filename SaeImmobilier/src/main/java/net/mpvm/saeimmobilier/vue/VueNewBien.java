a2411e781bbd4a30418c7116265a4e90b4c10694
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2025-01-08 15:49:16 +0100

    CtrlNewBien fonctionne

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueNewBien.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueNewBien.java
@@ -7,15 +7,12 @@ import net.mpvm.saeimmobilier.util.JfxUtil;
 
 public class VueNewBien extends Application {
 
-    private static final int DEFAULT_WIDTH = 850;
-    private static final int DEFAULT_HEIGHT = 750;
+
 
     @Override
     public void start(Stage stage) throws Exception {
 
-        JfxUtil.updateStage(stage, "newbien.fxml", "Ajouter un Bien", DEFAULT_HEIGHT, DEFAULT_WIDTH);
-        stage.setMinWidth(DEFAULT_WIDTH);
-        stage.setMinHeight(DEFAULT_HEIGHT);
+        JfxUtil.updateStage(stage, "newbien.fxml", "Ajouter un Bien", 750, 850);
     }
 }
 
