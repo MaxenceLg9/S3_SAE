0ba712eb03c0959c7f8481cf8206f4e3998c972d
Zeynnz <zzeynn8@gmail.com>
2025-01-10 15:58:22 +0100

    stop à la duplication et les mauvaises redirection

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/controleur/CtrlViewBiensLouables.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/controleur/CtrlViewBiensLouables.java
@@ -152,8 +152,6 @@ public class CtrlViewBiensLouables {
     }
 
     private void attribuerAssurance(int idBien,ActionEvent event) {
-        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
-        stage.close();
         Stage s = new Stage();
         s.getProperties().put("bien",idBien);
         new VueAttribuerAssurance(idBien).startForImmeuble(s);
