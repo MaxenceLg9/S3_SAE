d6ae0cf722f2cedd57385329fa9a964b936c2189
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2025-01-11 16:54:36 +0100

    implémentation start

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueNewLocataire.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueNewLocataire.java
@@ -4,10 +4,10 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import net.mpvm.saeimmobilier.util.JfxUtil;
 
-public class VueNewLocataire extends Application{
-
+public class VueNewLocataire extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        JfxUtil.updateStage(primaryStage, "newlocataire.fxml", "Ajout d'un Locataire", 750, 1200);
     }
 }
