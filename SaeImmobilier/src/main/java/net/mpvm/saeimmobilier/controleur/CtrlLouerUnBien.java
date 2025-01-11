8565c4142692752617687ca65357e3dc43034d4e
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-01-11 14:28:13 +0100

    ébauche de la fenetre LouerUnBien

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/controleur/CtrlLouerUnBien.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/controleur/CtrlLouerUnBien.java
@@ -1,7 +1,8 @@
 package net.mpvm.saeimmobilier.controleur;
 
 import javafx.application.Platform;
-import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -9,21 +10,37 @@ import net.mpvm.saeimmobilier.modele.BienLouable;
 
 public class CtrlLouerUnBien {
 
-    @FXML
-    public Label fieldAdresseBienL;
-    @FXML
-    private Label fieldNomBienL;
+    public DatePicker dateDebut;
+    public DatePicker dateFin;
+    public TextField fieldMontantLoyer;
+    public TextField fieldTotalCharges;
+    public TextField fieldDepotGarantie;
+    public CheckBox checkBoxArchive;
+    public CheckBox checkBoxRenouvelable;
+
+    public Label labelAdresse;
+    public Label labelNumeroFiscal;
+    public Label labelType;
+    public Label labelSurface;
+    public Label labelNbPieces;
+    public Label labelDateAjout;
+    public Label labelNomBienL;
 
     public void initialize(){
         Platform.runLater(() -> {
-            Stage stage = (Stage) fieldAdresseBienL.getScene().getWindow();
+            Stage stage = (Stage) labelAdresse.getScene().getWindow();
             BienLouable bienLouable = (BienLouable) stage.getProperties().get("bienLouable");
             if(bienLouable == null) {
                 stage.close();
                 return;
             }
-            fieldNomBienL.setText(bienLouable.getIdProprio());
-            fieldAdresseBienL.setText(bienLouable.getComplementAdresse() + " " + bienLouable.getAdresse() + "," + bienLouable.getVille());
+            labelNomBienL.setText("Nom " + bienLouable.getIdProprio());
+            labelAdresse.setText(bienLouable.getComplementAdresse() + ", " + bienLouable.getAdresse() + ", " + bienLouable.getCodePostal() + ", " + bienLouable.getVille());
+            labelNumeroFiscal.setText("N° fiscal " + bienLouable.getNumeroFiscal());
+            labelType.setText("Type " + bienLouable.getTypeBien().name());
+            labelSurface.setText("Surface " + bienLouable.getSurface());
+            labelNbPieces.setText(bienLouable.getNbPieces() + " pièces");
+            labelDateAjout.setText("Ajouté le " + bienLouable.getDateAjout().toString());
         });
     }
 
