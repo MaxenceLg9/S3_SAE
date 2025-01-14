8ee7f46b5eb23ebbaace1ccac4e74f1275c15d13
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-01-14 09:13:10 +0100

    changement avec les variables statiques de test

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueModifierBien.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/vue/VueModifierBien.java
@@ -2,12 +2,22 @@ package net.mpvm.saeimmobilier.vue;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import net.mpvm.saeimmobilier.modele.Bien;
+import net.mpvm.saeimmobilier.modele.Garage;
+import net.mpvm.saeimmobilier.modele.Habitation;
+import net.mpvm.saeimmobilier.modele.Immeuble;
 import net.mpvm.saeimmobilier.util.JfxUtil;
+
+import java.sql.Date;
+import java.time.LocalDate;
 
 public class VueModifierBien extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        if(!stage.getProperties().containsKey("bien") || !(stage.getProperties().get("bien") instanceof Bien)) {
+            stage.getProperties().put("bien", Garage.GARAGE);
+        }
         JfxUtil.updateStage(stage, "modifierBien.fxml", "Modifier un Bien", 750, 850);
     }
 }
