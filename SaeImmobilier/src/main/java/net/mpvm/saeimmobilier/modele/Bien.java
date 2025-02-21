a3d6487ea5f30f33d62b2110624c70ef54d5d406
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-02-21 19:37:41 +0100

    Refactoring & suppression de code mort

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Bien.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Bien.java
@@ -5,7 +5,6 @@ import net.mpvm.saeimmobilier.sql.Query.Queryable;
 import net.mpvm.saeimmobilier.sql.Query.SelectQueryElement;
 import net.mpvm.saeimmobilier.sql.Query.UpdateQueryElement;
 
-import java.math.BigInteger;
 import java.sql.Date;
 import java.sql.*;
 import java.util.*;
@@ -14,7 +13,6 @@ import java.util.*;
 public abstract class Bien extends Queryable {
 
     private static final String SELECT_QUERY = "SELECT * FROM bien";
-    private static final String SELECT_ID_QUERY = "SELECT IdBien FROM bien WHERE NumeroFiscal = ?";
     private static final String SELECT_QUERY_BY_ID = "SELECT * FROM bien WHERE IdBien = ?";
     public static final String SELECT_FROM_ID = "SELECT * FROM Bien WHERE IdBien = ?";
 
@@ -70,8 +68,9 @@ public abstract class Bien extends Queryable {
         return Optional.empty();
     }
 
-    public void setAssurance(Assurance assurance) {
+    public void setAssurance(Assurance assurance) throws Assurance.AssuranceException {
         //TODO : query
+        assurance.attribuerUneAssurance(this);
     }
 
     //fonction pour "sauvegarder" l'id générée par
@@ -122,8 +121,6 @@ public abstract class Bien extends Queryable {
         return getTypeBien().name();
     }
 
-    abstract Map<String, Object> getArgs();
-
     public void setIdProprio(String idProprio) {
         this.idProprio = idProprio;
     }
@@ -148,9 +145,6 @@ public abstract class Bien extends Queryable {
     @Override
     public void archiver() {
 
-    }
-
-    public void update() {
     }
 
     public String getIdProprio() {
