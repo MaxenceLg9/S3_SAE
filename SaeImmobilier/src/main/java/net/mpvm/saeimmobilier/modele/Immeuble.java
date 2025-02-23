3669d62c5f4c38750022afcff9181b27a55af009
Navarro Valentin <valentin.navarro@etu.iut-tlse3.fr>
2025-02-23 12:55:58 +0100

    Suppression en cascade des travaux, assurances et des biens réparée

--- SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Immeuble.java
+++ SaeImmobilier/src/main/java/net/mpvm/saeimmobilier/modele/Immeuble.java
@@ -21,13 +21,13 @@ public final class Immeuble extends Bien{
 			throw new RuntimeException(e);
 		}
 	}
-	public static final String SELECT_QUERY_ID = "SELECT * FROM Bien WHERE IdBien = ? AND TypeBien = 'IMMEUBLE'";
+
 	public static final String INSERT_QUERY = "INSERT INTO Bien (Adresse, Ville, CodePostal, TypeBien, NumeroFiscal, IdProprio, DateAjout) VALUES (?, ?, ?, ?, ?, ?, ?)";
 	public static final String SELECT_QUERY = "SELECT * FROM Bien WHERE TypeBien = 'IMMEUBLE'";
 	public static final String SELECT_WHERE_QUERY = "SELECT * FROM Bien WHERE Adresse = ? AND Ville = ? AND CodePostal = ?";
 	public static final String DELETE_QUERY = "DELETE FROM Bien WHERE IdBien = ? AND TypeBien = 'IMMEUBLE'";
 	public static final String UPDATE_QUERY = "UPDATE Bien SET Adresse = ?, Ville = ?, CodePostal = ?, IdProprio = ? WHERE IdBien = ?";
-	public static final String SELECT_BIENS_IMMEUBLES = "SELECT * FROM Bien WHERE IdImmeuble = ? AND (TypeBien = 'GARAGE' OR TypeBien = 'HABITATION')";
+	public static final String SELECT_BIENS_IMMEUBLES = "SELECT * FROM Bien WHERE IdImmeuble = ? AND TypeBien = 'GARAGE' OR TypeBien = 'HABITATION'";
 	public static final String SELECT_COUNT_BL = "SELECT Count(*) FROM Bien WHERE IdImmeuble = ? AND (TypeBien = 'GARAGE' OR TypeBien = 'HABITATION')";
 	public static final String SELECT_LOCALISATION = "SELECT IdImmeuble FROM Bien WHERE Adresse = ? AND Ville = ? AND CodePostal = ? AND TypeBien = 'IMMEUBLE'";
 
@@ -97,9 +97,9 @@ public final class Immeuble extends Bien{
 		return TypeBien.IMMEUBLE;
 	}
 
-//	public List<Travaux> getTravauxAssocies() throws Travaux.TravauxException {
-//		return Travaux.getTravauxFromImmeuble(this);
-//	}
+	public List<Travaux> getTravauxAssocies() throws Travaux.TravauxException {
+		return Travaux.getTravauxFromImmeuble(this);
+	}
 
 	public List<BienLouable> getBiensAssocies() throws ImmeubleException {
 		List<BienLouable> bienLouablesAssocies = new LinkedList<>();
@@ -191,26 +191,21 @@ public final class Immeuble extends Bien{
 	}
 
 	@Override
-	public void delete() throws ImmeubleException, BienLouable.BienLouableException {
+	public void delete() throws ImmeubleException {
 		if (this.getIdBien() == -1){
 			throw new ImmeubleException("Le bien n'existe pas dans la table", null);
 		}
-		for (BienLouable b :this.getBiensAssocies()){
-			b.delete();
-		}
+
 
 
 		try (UpdateQueryElement deleteQuery = new UpdateQueryElement(DELETE_QUERY, true)) {
 			super.delete();
-			deleteQuery.setArgs(Map.of(1, this.getIdBien())).execute();
-
 		}
-		 catch (QueryElement.QEltException e) {
+		catch (QueryElement.QEltException e) {
 			e.getSqlException().printStackTrace();
 			throw new ImmeubleException("Erreur lors de la suppression du bien", e.getSqlException());
 		}
 	}
-
 
 	@Override
 	public String toString(){
